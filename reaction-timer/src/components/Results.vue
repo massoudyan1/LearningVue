fb8c89c8258ce226c3eb82a66d2993c095188462
massoudyan1 <47738720+massoudyan1@users.noreply.github.com>
2021-03-23 14:56:07 +0100

    SignupForm ongoing
    Done with Sayan Timer

---
+++ reaction-timer/src/components/Results.vue
@@ -0,0 +1,30 @@
+<template>
+  <p class="rank">Reaction time: {{ score }} ms</p>
+  <p class="rank">Ranking: {{ rank }}</p>
+</template>
+
+<script>
+export default {
+    props: ['score'],
+    data(){
+        return {
+            rank: null
+        }
+    },
+    mounted() {
+        if(this.score < 250)
+            this.rank = 'Goku Fingers'
+        else if(this.score < 400)
+            this.rank = 'Vegeta Reflexes'
+        else
+            this.rank = 'Hercule Pace'
+    }
+}
+</script>
+
+<style>
+    .rank{
+        font-size: 1.4em;
+        color: rgb(70,150,120);
+    }
+</style>
